fafcf7ddfbcbc9f1060f36017a10b9dcdb9675a2
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2024-01-30 10:29:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,3 +9,11 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+
+# Rust-host test
+git clone --depth=1 -b openwrt-23.05 https://github.com/immortalwrt/packages packages-temp
+echo "download rust/host form immortalwrt/packages-openwrt-23.05"
+cp -r packages-temp/lang/rust feeds/packages/lang
+rm -rf packages-temp
+ls -a
+echo "Rust/host $(grep "PKG_VERSION" feeds/packages/lang/rust/Makefile)"
