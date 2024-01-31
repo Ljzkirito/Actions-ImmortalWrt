6e12f78b4a85e18e5db352181e7588011ae5d166
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2024-01-31 22:01:20 +0800

    Replace passwall & Depends

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,30 +15,34 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
 # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
-rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/Ljzkirito/v2ray-geodata feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/net/mosdns
-find ./ | grep Makefile | grep luci-app-mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# rm -rf feeds/packages/net/v2ray-geodata
+# git clone https://github.com/Ljzkirito/v2ray-geodata feeds/packages/net/v2ray-geodata
+# rm -rf feeds/packages/net/mosdns
+# find ./ | grep Makefile | grep luci-app-mosdns | xargs rm -f
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 
 # Replace Smartdns
 rm -rf feeds/packages/net/smartdns
 git clone https://github.com/Ljzkirito/smartdns-openwrt feeds/packages/net/smartdns
 
-# Replace luci-app-ssr-plus & Depends
-git clone --depth=1 -b master https://github.com/fw876/helloworld
-Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2socks microsocks redsocks2 chinadns-ng dns2socks dns2tcp naiveproxy simple-obfs tcping tuic-client"
+# Replace luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications
+
+# Replace passwall Depends
+git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall-packages
+Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin brook chinadns-ng dns2socks dns2tcp hysteria ipt2socks microsocks naiveproxy pdnsd-alt shadowsocksr-libev simple-obfs tcping trojan-go trojan-plus trojan tuic-client"
 for a in ${Replace_package}
 do
 	echo "Replace_package=$a"
  	rm -rf feeds/packages/net/"$a"
-	cp -r helloworld/"$a" feeds/packages/net
+	cp -r openwrt-passwall-packages/"$a" feeds/packages/net
 done
 # sed -i 's/ +libopenssl-legacy//g' feeds/packages/net/shadowsocksr-libev/Makefile
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-cp -r helloworld/luci-app-ssr-plus feeds/luci/applications
-cp -r helloworld/shadow-tls package
-rm -rf helloworld
+rm -rf feeds/packages/devel/gn && cp -r openwrt-passwall-packages/gn feeds/packages/devel
+cp -r openwrt-passwall-packages/sing-box package
+cp -r openwrt-passwall-packages/ssocks package
+rm -rf openwrt-passwall-packages
 
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
