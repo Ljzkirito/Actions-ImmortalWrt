3deb90c9038b412462ae03a67d0788dbe2062432
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2024-02-03 19:42:53 +0800

    Replace Smartdns conf

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,19 +11,19 @@
 #
 
 # golang 1.21.x
-rm -rf feeds/packages/lang/golang
+rm -rfv feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
 # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
-rm -rf feeds/packages/net/v2ray-geodata
+rm -rfv feeds/packages/net/v2ray-geodata
 git clone https://github.com/Ljzkirito/v2ray-geodata feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/net/mosdns
-find ./ | grep Makefile | grep luci-app-mosdns | xargs rm -f
+rm -rfv feeds/packages/net/mosdns
+find ./ | grep Makefile | grep luci-app-mosdns | xargs rm -fv
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 
-# Replace Smartdns
-rm -rf feeds/packages/net/smartdns
-git clone https://github.com/Ljzkirito/smartdns-openwrt feeds/packages/net/smartdns
+# Replace Smartdns conf
+cp -fv $GITHUB_WORKSPACE/smartdns-conf/custom.conf feeds/packages/net/smartdns/conf
+cp -fv $GITHUB_WORKSPACE/smartdns-conf/smartdns.conf feeds/packages/net/smartdns/conf
 
 # Replace luci-app-ssr-plus & Depends
 git clone --depth=1 -b master https://github.com/fw876/helloworld
@@ -31,14 +31,14 @@ Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2sock
 for a in ${Replace_package}
 do
 	echo "Replace_package=$a"
- 	rm -rf feeds/packages/net/"$a"
-	cp -r helloworld/"$a" feeds/packages/net
+ 	rm -rfv feeds/packages/net/"$a"
+	cp -rv helloworld/"$a" feeds/packages/net
 done
 # sed -i 's/ +libopenssl-legacy//g' feeds/packages/net/shadowsocksr-libev/Makefile
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-cp -r helloworld/luci-app-ssr-plus feeds/luci/applications
-cp -r helloworld/shadow-tls package
-rm -rf helloworld
+rm -rfv feeds/luci/applications/luci-app-ssr-plus
+cp -rv helloworld/luci-app-ssr-plus feeds/luci/applications
+cp -rv helloworld/shadow-tls package
+rm -rfv helloworld
 
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
