effbc60de29413b82b24c02919e91cc9e290eb46
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2024-02-07 10:14:48 +0800

    Set nf_conntrack_max=16384

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,3 +23,7 @@ while true; do
   fi
 done
 EOF
+#按照mtk-sdk和最新openwrt主线的有线驱动，mt7981的ADMAv2存在不可修复的问题，immortalwrt-mt798x已参照上述源码将mt7981 ADMAv2回退至ADMAv1。
+#当有线驱动使用ADMAv1时，每个PPE最多支持16384个Entry。每个NAT连接需要占用2个Entry（进站和出站方向）。
+sed -i 's/nf_conntrack_max=65536/nf_conntrack_max=16384/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+sed -i 's/nf_conntrack_buckets=65536/nf_conntrack_buckets=16384/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
