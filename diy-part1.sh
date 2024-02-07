6ea47847e1743099079fef12fb8a1c7237f7cbd7
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2024-02-07 10:00:31 +0800

    Replace /lib/netifd/ppp-down

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,3 +9,17 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+
+# Replace /lib/netifd/ppp-down
+# https://github.com/hanwckf/immortalwrt-mt798x/issues/57
+cat >> package/network/services/ppp/files/lib/netifd/ppp-down <<EOF
+while true; do
+  if ubus -S list "network.interface.wan_6"; then
+    # ubus call network.interface.wan_6 down
+    logger "waiting wan_6 down..."
+    sleep 1
+  else
+    break
+  fi
+done
+EOF
