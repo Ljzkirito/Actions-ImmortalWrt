091cef4d76bed6b4550b18d441271a087208d829
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2024-02-16 21:40:54 +0800

    feeds install -f -p helloworld

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,22 +26,25 @@ cp -fv $GITHUB_WORKSPACE/smartdns-conf/custom.conf feeds/packages/net/smartdns/c
 cp -fv $GITHUB_WORKSPACE/smartdns-conf/smartdns.conf feeds/packages/net/smartdns/conf
 
 # Replace luci-app-ssr-plus & Depends
-git clone --depth=1 -b master https://github.com/fw876/helloworld
-Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2socks microsocks redsocks2 chinadns-ng dns2socks dns2tcp naiveproxy simple-obfs tcping tuic-client"
-for a in ${Replace_package}
-do
-	echo "Replace_package=$a"
- 	rm -rfv feeds/packages/net/"$a"
-	cp -rv helloworld/"$a" feeds/packages/net
-done
-# sed -i 's/ +libopenssl-legacy//g' feeds/packages/net/shadowsocksr-libev/Makefile
-rm -rfv feeds/luci/applications/luci-app-ssr-plus
-cp -rv helloworld/luci-app-ssr-plus feeds/luci/applications
-cp -rv helloworld/shadow-tls package
-rm -rfv helloworld
+Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2socks microsocks redsocks2 chinadns-ng dns2socks dns2tcp naiveproxy simple-obfs tcping tuic-client luci-app-ssr-plus"
+./scripts/feeds uninstall ${Replace_package}
+./scripts/feeds install -f -p helloworld ${Replace_package}
 
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
+#git clone --depth=1 -b master https://github.com/fw876/helloworld
+#Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2socks microsocks redsocks2 chinadns-ng dns2socks dns2tcp naiveproxy simple-obfs tcping tuic-client"
+#for a in ${Replace_package}
+#do
+#	echo "Replace_package=$a"
+# 	rm -rfv feeds/packages/net/"$a"
+#	cp -rv helloworld/"$a" feeds/packages/net
+#done
+# sed -i 's/ +libopenssl-legacy//g' feeds/packages/net/shadowsocksr-libev/Makefile
+#rm -rfv feeds/luci/applications/luci-app-ssr-plus
+#cp -rv helloworld/luci-app-ssr-plus feeds/luci/applications
+#cp -rv helloworld/shadow-tls package
+#rm -rfv helloworld
 # Remove upx commands
 #makefile_file="$({ find package|grep Makefile |sed "/Makefile./d"; } 2>"/dev/null")"
 #for a in ${makefile_file}
