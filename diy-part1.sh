cfdafd6315ff6d7da57f8a69ba1f9f30c295e2db
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2024-10-18 20:25:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,22 +10,8 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
 echo "src-git customsd https://github.com/Ljzkirito/smartdns-openwrt.git;Release46-conf" >> "feeds.conf.default"
 
-# Replace /lib/netifd/ppp-down
-# https://github.com/hanwckf/immortalwrt-mt798x/issues/57
-#cat >> package/network/services/ppp/files/lib/netifd/ppp-down <<EOF
-#while true; do
-#  if ubus -S list "network.interface.wan_6"; then
-#    # ubus call network.interface.wan_6 down
-#    logger "waiting wan_6 down..."
-#    sleep 1
-#  else
-#    break
-#  fi
-#done
-#EOF
 # 取消Dnsmasq缓存，由smartdns负责。不修改ttl
 sed -i 's/cachesize\t8000/cachesize\t0/g' package/network/services/dnsmasq/files/dhcp.conf
 sed -i 's/mini_ttl\t3600/mini_ttl\t0/g' package/network/services/dnsmasq/files/dhcp.conf
