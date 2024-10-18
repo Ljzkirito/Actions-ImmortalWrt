b485dbc95501db3cfc09a2357d2ef9970caf521c
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2024-10-18 20:26:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,52 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# golang 1.23.x
-rm -rfv feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-
-# remove v2ray-geodata package from feeds (openwrt-22.03 & master)
-rm -rfv feeds/packages/net/v2ray-geodata
-git clone https://github.com/Ljzkirito/v2ray-geodata feeds/packages/net/v2ray-geodata
-rm -rfv feeds/packages/net/mosdns
-find ./ | grep Makefile | grep luci-app-mosdns | xargs rm -fv
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-
 # Replace Smartdns
 ./scripts/feeds uninstall luci-app-smartdns smartdns
 ./scripts/feeds install -a -p customsd
 
-# Replace luci-app-ssr-plus & Depends
-Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2socks microsocks redsocks2 chinadns-ng dns2socks dns2tcp naiveproxy simple-obfs tcping tuic-client luci-app-ssr-plus"
-./scripts/feeds uninstall ${Replace_package}
-./scripts/feeds install -f -p helloworld ${Replace_package}
-
-# Replace shadowsocks-rust
-rm -rfv feeds/packages/net/shadowsocks-rust
-git clone https://github.com/Ljzkirito/shadowsocks-rust feeds/packages/net/shadowsocks-rust
-
-# Replace adguardhome
-rm -rfv feeds/packages/net/adguardhome
-git clone https://github.com/Ljzkirito/adguardhome-openwrt feeds/packages/net/adguardhome
-
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-
-#git clone --depth=1 -b master https://github.com/fw876/helloworld
-#Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2socks microsocks redsocks2 chinadns-ng dns2socks dns2tcp naiveproxy simple-obfs tcping tuic-client"
-#for a in ${Replace_package}
-#do
-#	echo "Replace_package=$a"
-# 	rm -rfv feeds/packages/net/"$a"
-#	cp -rv helloworld/"$a" feeds/packages/net
-#done
-# sed -i 's/ +libopenssl-legacy//g' feeds/packages/net/shadowsocksr-libev/Makefile
-#rm -rfv feeds/luci/applications/luci-app-ssr-plus
-#cp -rv helloworld/luci-app-ssr-plus feeds/luci/applications
-#cp -rv helloworld/shadow-tls package
-#rm -rfv helloworld
-# Remove upx commands
-#makefile_file="$({ find package|grep Makefile |sed "/Makefile./d"; } 2>"/dev/null")"
-#for a in ${makefile_file}
-#do
-#	[ -n "$(grep "upx" "$a")" ] && sed -i "/upx/d" "$a"
-#done
