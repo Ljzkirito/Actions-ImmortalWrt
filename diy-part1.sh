1f3c4b4b8efa237c8b7a16f5f2cfcfd19edbf593
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2026-06-17 12:02:35 +0800

    Increase mt7981 token_rx_cnt in patch
    Append a patch to diy-part1.sh that adds/updates package/mtk/drivers/mt_wifi/patches-7673/022-Increase-token-rx-cnt.patch to change mt7981 token_rx_cnt from 4592 to 6144 under MEMORY_SHRINK_AGGRESS. Also re-adds removal of package/emortal/ipv6-helper/files/60-6in4. This increases RX token count to improve Wi-Fi behavior when MEMORY_SHRINK_AGGRESS is enabled.

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,17 @@
 sed -i 's/ +6in4//g' package/emortal/ipv6-helper/Makefile
 sed -i '/hotplug.d/d' package/emortal/ipv6-helper/Makefile
 rm -fv package/emortal/ipv6-helper/files/60-6in4
+
+cat >> package/mtk/drivers/mt_wifi/patches-7673/022-Increase-token-rx-cnt.patch <<EOF
+--- a/mt_wifi/chips/mt7981.c
++++ b/mt_wifi/chips/mt7981.c
+@@ -11711,7 +11711,7 @@
+ 	chip_cap->tkn_info.hw_tx_token_cnt = 8192;
+ #ifdef MEMORY_SHRINK
+ #ifdef MEMORY_SHRINK_AGGRESS
+-	chip_cap->tkn_info.token_rx_cnt = 4592;
++	chip_cap->tkn_info.token_rx_cnt = 6144;
+ #else
+ 	chip_cap->tkn_info.token_rx_cnt = 12288;
+ #endif	/* MEMORY_SHRINK_AGGRESS */
+EOF
