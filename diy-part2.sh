17581db1b400039479ae35c83973f7b72095be02
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2026-06-21 20:02:42 +0800

    Use jerrykuku luci-theme-argon commit in clone
    Update diy-part2.sh to replace the luci-theme-argon clone source: previously cloned Ljzkirito repo on branch 'test1', now clones the jerrykuku repository using a specific commit SHA as the ref. This pins the theme source to the jerrykuku repo at the given revision.

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,7 +20,7 @@ rm -rf temp-smartdns
 
 # Replace luci-theme-argon
 rm -rfv feeds/luci/themes/luci-theme-argon
-git clone -b test1 https://github.com/Ljzkirito/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone -b 1991a8e29ef6a086fb566517675edc85b1be629a https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
